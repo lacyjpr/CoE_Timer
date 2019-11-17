4ecb03599da60f0d7b10fea8c78e7aab3db25cb1
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-11-16 18:53:32 -0800

    Fix eslint indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
-        "jquery": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "no-console":0,
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jquery: true
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "no-console": 0,
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
 };
